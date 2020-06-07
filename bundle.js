26d456e686cbb2423dbc4c55e5ea4dd24ac57e36
Carlos Augusto Arantes <carl.systemsbr@gmail.com>
2020-06-07 20:22:25 -0300

    estudando desestruturação de objetos

--- bundle.js
+++ bundle.js
@@ -1,17 +1,39 @@
 "use strict";
 
-var arr = [1, 3, 4, 5, 6];
-var newArr = arr.map(function (item) {
-  return item * 2;
-});
+/*
+const arr = [1, 3, 4, 5, 6];
+
+const newArr = arr.map(item => item * 2);
+
 console.log(newArr);
 
-var teste = function teste() {
-  return {
-    nome: "Doido"
-  };
-};
+const teste = () => ({ nome: "Doido" });
 
 function soma(a, b) {
-  return a + b;
+    return a + b;
 }
+*/
+// Desestruturação de objetos
+var usuario = {
+  nome: "Diego",
+  idade: 23,
+  endereco: {
+    cidade: "Rio do Sul",
+    estado: "SC"
+  }
+};
+/*
+const nome   = usuario.nome;
+const idade  = usuario.idade;
+const cidade = usuario.endereco.cidade;
+
+const { nome, idade, endereco: { cidade } } = usuario;
+*/
+
+function mostraNome(_ref) {
+  var nome = _ref.nome,
+      idade = _ref.idade;
+  console.log(nome, idade);
+}
+
+mostraNome(usuario);
--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 const arr = [1, 3, 4, 5, 6];
 
 const newArr = arr.map(item => item * 2);
@@ -9,3 +10,29 @@ const teste = () => ({ nome: "Doido" });
 function soma(a, b) {
     return a + b;
 }
+*/
+
+
+// Desestruturação de objetos
+const usuario = {
+    nome : "Diego",
+    idade: 23,
+    endereco: {
+        cidade: "Rio do Sul",
+        estado: "SC",
+    }
+}
+
+/*
+const nome   = usuario.nome;
+const idade  = usuario.idade;
+const cidade = usuario.endereco.cidade;
+
+const { nome, idade, endereco: { cidade } } = usuario;
+*/
+
+function mostraNome({ nome, idade }) {
+    console.log(nome, idade);
+}
+
+mostraNome(usuario);
