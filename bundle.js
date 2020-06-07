75f68d201be72be7a900290d391b26976f931870
Carlos Augusto Arantes <carl.systemsbr@gmail.com>
2020-06-07 20:13:54 -0300

    initial

---
+++ bundle.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var arr = [1, 3, 4, 5, 6];
+var newArr = arr.map(function (item) {
+  return item * 2;
+});
+console.log(newArr);
+
+var teste = function teste() {
+  return {
+    nome: "Doido"
+  };
+};
+
+function soma(a, b) {
+  return a + b;
+}
---
+++ main.js
@@ -0,0 +1,11 @@
+const arr = [1, 3, 4, 5, 6];
+
+const newArr = arr.map(item => item * 2);
+
+console.log(newArr);
+
+const teste = () => ({ nome: "Doido" });
+
+function soma(a, b) {
+    return a + b;
+}
