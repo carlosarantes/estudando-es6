ce187f52678b0b6c0bb2cec2baec7c4e8decdbf5
Carlos Augusto Arantes <carl.systemsbr@gmail.com>
2020-06-07 22:46:10 -0300

    rest/spread

--- bundle.js
+++ bundle.js
@@ -1,5 +1,15 @@
 "use strict";
 
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+
 /*
 const arr = [1, 3, 4, 5, 6];
 
@@ -14,26 +24,62 @@ function soma(a, b) {
 }
 */
 // Desestruturação de objetos
-var usuario = {
-  nome: "Diego",
-  idade: 23,
-  endereco: {
-    cidade: "Rio do Sul",
-    estado: "SC"
-  }
-};
+
 /*
+const usuario = {
+    nome : "Diego",
+    idade: 23,
+    endereco: {
+        cidade: "Rio do Sul",
+        estado: "SC",
+    }
+}
+
+
 const nome   = usuario.nome;
 const idade  = usuario.idade;
 const cidade = usuario.endereco.cidade;
 
 const { nome, idade, endereco: { cidade } } = usuario;
-*/
 
-function mostraNome(_ref) {
-  var nome = _ref.nome,
-      idade = _ref.idade;
-  console.log(nome, idade);
+
+function mostraNome({ nome, idade }) {
+    console.log(nome, idade);
 }
 
 mostraNome(usuario);
+*/
+var usuario = {
+  nome: "Doido",
+  idade: 25,
+  empresa: "iHealth"
+}; // rest
+
+var nome = usuario.nome,
+    resto = _objectWithoutProperties(usuario, ["nome"]);
+
+function soma() {
+  for (var _len = arguments.length, params = new Array(_len), _key = 0; _key < _len; _key++) {
+    params[_key] = arguments[_key];
+  }
+
+  return params.reduce(function (total, next) {
+    return total + next;
+  });
+} // spread
+
+
+var arr1 = [1, 2, 3];
+var arr2 = [4, 5, 6];
+var arr3 = [].concat(arr1, arr2);
+var usuario1 = {
+  nome: "Carlos",
+  idade: 23,
+  empresa: "RocketSeat"
+};
+
+var usuario2 = _objectSpread(_objectSpread({}, usuario1), {}, {
+  nome: "Doido"
+});
+
+console.log(usuario2);
--- main.js
+++ main.js
@@ -14,6 +14,7 @@ function soma(a, b) {
 
 
 // Desestruturação de objetos
+/*
 const usuario = {
     nome : "Diego",
     idade: 23,
@@ -23,16 +24,46 @@ const usuario = {
     }
 }
 
-/*
+
 const nome   = usuario.nome;
 const idade  = usuario.idade;
 const cidade = usuario.endereco.cidade;
 
 const { nome, idade, endereco: { cidade } } = usuario;
-*/
+
 
 function mostraNome({ nome, idade }) {
     console.log(nome, idade);
 }
 
 mostraNome(usuario);
+*/
+
+const usuario = {
+    nome: "Doido",
+    idade: 25,
+    empresa: "iHealth"
+};
+
+// rest
+const { nome, ...resto } = usuario;
+
+function soma(...params) {
+    return params.reduce((total, next) => total + next);    
+}
+
+// spread
+const arr1 = [1,2,3];
+const arr2 = [4,5,6];
+
+const arr3 = [ ...arr1, ...arr2 ];
+
+const usuario1 = {
+    nome: "Carlos",
+    idade: 23,
+    empresa: "RocketSeat"
+};
+
+const usuario2 = { ...usuario1, nome: "Doido" };
+
+console.log(usuario2);
