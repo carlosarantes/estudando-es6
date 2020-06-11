9d1b7afd4cc58f016737ed5fd712aebdca77f06d
Carlos Augusto Arantes <carl.systemsbr@gmail.com>
2020-06-11 18:21:35 -0300

    estudando es6

--- src/main.js
+++ src/main.js
@@ -1,14 +1,54 @@
-const minhaPromisse = () => new Promise((resolve, reject) => {
-    setTimeout( () => {
-        resolve('OK');
-    }, 5000);
-});
+class App {
+    constructor() {
+        this.repositories = [];
+        this.formEl       = document.getElementById('repo-form');
+        this.listEl       = document.getElementById('repo-list');
+        this.registerHandlers();
+    }
 
-async function executaPromisse() {
+    registerHandlers() {
+        this.formEl.onsubmit = event => this.addRepository(event);
+    }
 
-    const response = await minhaPromisse();
+    addRepository(event) {
+        event.preventDefault();
 
-    console.log('response ** ', response)
+        this.repositories.push({
+            name: "rocketseat.com.br",
+            description: "Tire sua idéia do papel e dê vida a sua startup.",
+            avatar_url: "https://avatars0.githubusercontent.com/u/28929274?v=4",
+            html_url: "http://github.com/rocketseat/rocketseat.com.br"
+        });
+        
+        this.render();
+    }
+
+    render() {
+        this.listEl.innerHTML = "";
+
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild( document.createTextNode(repo.description) );
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.appendChild( document.createTextNode('Acessar') );
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+        });
+    }
 }
 
-executaPromisse();
+new App();
