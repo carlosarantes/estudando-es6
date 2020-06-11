b346a8829302219b31de129eb9cd41968108a14c
Carlos Augusto Arantes <carl.systemsbr@gmail.com>
2020-06-11 17:03:10 -0300

    import export

--- bundle.js
+++ bundle.js
@@ -1,189 +1,113 @@
-/*
-    DESAFIO 1
-    ---------------------------------
-    class Usuario {
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./main.js");
+/******/ })
+/************************************************************************/
+/******/ ({
 
-        constructor(email, senha){
-            this.email = email;
-            this.senha = senha;
-            this.setAdmin(false);
-        }
+/***/ "./funcoes.js":
+/*!********************!*\
+  !*** ./funcoes.js ***!
+  \********************/
+/*! exports provided: soma, sub, multi */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-        isAdmin() {
-            return this.admin;
-        }
-
-        setAdmin(admin){
-            this.admin = admin;
-        }
-    }
-
-    class Admin extends Usuario {
-
-        constructor(email, senha){
-            super(email, senha);
-            this.setAdmin(true);
-        }
-    }
-
-    const User1 = new Usuario('email@teste.com', 'senha123');
-    const Adm1 = new Admin('email@teste.com', 'senha123');
-
-    console.log(User1.isAdmin()) // false
-    console.log(Adm1.isAdmin()) // true
-
-    ================================================
-    DESAFIO 2
-    ---------------------------------
-
-
-    // Obtendo as idades dos usuarios
-    const usuarios = [
-        { nome: 'Diego', idade: 23, empresa: 'Rocketseat' },
-        { nome: 'Gabriel', idade: 15, empresa: 'Rocketseat' },
-        { nome: 'Lucas', idade: 30, empresa: 'Facebook' },
-    ];
-
-    const idades = usuarios.map((usuario) => usuario.idade );
-    console.log(idades);
-
-    // Usuarios que trabalham na Rocketseat com mais de 18 anos
-    const empregadosRocketseat = usuarios.filter( usuario => (usuario.empresa == 'Rocketseat' && usuario.idade > 18) );
-    console.log(empregadosRocketseat);
-
-    // Usuario que trabalha na empresa google
-    const funcionarioGoogle = usuarios.find( usuario => usuario.empresa == 'Google');
-    console.log(funcionarioGoogle);
-
-    // Usuarios ficando mais velhos
-    const maisVelhos = usuarios.map( (usuario) => {
-        usuario.idade *= 2;
-        return usuario;
-    }).filter(usurio => usurio.idade <= 50);
-    console.log(maisVelhos);
-
-    ================================================
-    DESAFIO 3
-    ---------------------------------
-
-    // Tranformar em arrow function
-    const arr = [1, 2, 3, 4, 5];
-    const itens = arr.map(item =>item + 10);
-    console.log(itens);
-
-    const usuario = { nome: 'Diego', idade: 23 };
-    const mostraIdade = (usuario) => usuario.idade;
-    console.log("Idade: ", mostraIdade(usuario));
-
-
-
-    const nome = "Diego";
-    const idade = 23;
-    const mostraUsuario = (nome = 'Diego', idade = 18) => ({ nome, idade });
-
-    console.log(mostraUsuario(nome, idade));
-    console.log(mostraUsuario(nome));
-
-
-    const promise = () => new Promise((resolve, reject) => resolve());
-
-    
-    ================================================
-    DESAFIO 4
-    ---------------------------------
-
-    const empresa = {
-        nome: 'Rocketseat',
-        endereco: {
-            cidade: 'Rio do Sul',
-            estado: 'SC',
-        }
-    };
-
-    const { nome, endereco: { cidade, estado }  } = empresa; 
-
-    console.log(nome); // Rocketseat
-    console.log(cidade); // Rio do Sul
-    console.log(estado); 
-    
-    function mostraInfo({ nome, idade }) {
-        return `${nome} tem ${idade} anos.`;
-    }
-    console.log(   mostraInfo({ nome: 'Carlota', idade: 25 })  );
-
-
-    ================================================
-    DESAFIO 5
-    ---------------------------------
-
-    // REST/SPREAD
-
-    const arr = [1, 2, 3, 4, 5, 6];
-
-    const [ x, ...y ] = arr;
-
-    console.log(x); // 1
-    console.log(y); // [2, 3, 4, 5, 6]
-
-
-
-
-
-    // function soma...
-    const soma = (...params) => params.reduce( (total, next) => total + next);
-
-    console.log(soma(1, 2, 3, 4, 5, 6)); // 21
-    console.log(soma(1, 2)); //
-
-
-    const usuario = {
-        nome: 'Diego',
-        idade: 23,
-        endereco: {
-            cidade: 'Rio do Sul',
-            uf: 'SC',
-            pais: 'Brasil',
-        }
-    };
-
-    const usuario2 = {
-        ...usuario,
-        nome: "Gabriel"
-    };
-
-    const usuario3 = {
-        ...usuario,
-        nome: "Lontras"
-    };
-
-    console.log(usuario2);
-    console.log(usuario3);
-
-
-    ================================================
-    DESAFIO 6
-    ---------------------------------
-
-    // TEMPLATE LITERALS
-
-    const usuario = 'Diego';
-    const idade = 23;
-    console.log(`O usuário ${usuario} possui ${idade} anos`);
-
-
-    ================================================
-    DESAFIO 7
-    ---------------------------------
-
-    // Sintaxe curta de objetos
-
-    const nome = 'Diego';
-    const idade = 23;
-    const cidade = 'Rio do Sul',
-    const usuario = {
-        nome,
-        idade,
-        cidade
-    };
- */
 "use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"soma\", function() { return soma; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"sub\", function() { return sub; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"multi\", function() { return multi; });\nfunction soma(a, b) {\n  return a + b;\n}\nfunction sub(a, b) {\n  return a - b;\n}\nfunction multi(a, b) {\n  return a * b;\n}\n\n//# sourceURL=webpack:///./funcoes.js?");
+
+/***/ }),
+
+/***/ "./main.js":
+/*!*****************!*\
+  !*** ./main.js ***!
+  \*****************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _funcoes_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./funcoes.js */ \"./funcoes.js\");\n // import { soma, sub, multi } from \"./funcoes.js\";\n// import soma from \"./soma\";\n\nconsole.log(soma(1, 2));\nconsole.log(sub(3, 2));\n\n//# sourceURL=webpack:///./main.js?");
+
+/***/ })
+
+/******/ });
---
+++ funcoes.js
@@ -0,0 +1,11 @@
+export function soma (a, b) {
+    return a + b;
+}
+
+export function sub(a, b) {
+    return a - b;
+}
+
+export function multi(a, b) {
+    return a * b;
+}
